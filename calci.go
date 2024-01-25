8e2e89908f245b68ed6ee1676c701a602e083914
Rutik <rutikgagare091971@gmail.com>
2024-01-25 12:44:54 +0530

    Update divide func with error

--- calci.go
+++ calci.go
@@ -1,5 +1,7 @@
 package calci
 
+import "errors"
+
 func Add(num1 int, num2 int) int {
 	return num1 + num2
 }
@@ -12,6 +14,11 @@ func Multiply(num1, num2 int) int {
 	return num1 * num2
 }
 
-func Divide(num1, num2 int) int {
-	return num1 / num2
+func Divide(num1, num2 int) (int, error) {
+
+	if num2 == 0 {
+		return 0, errors.New("Divide by Zero")
+	}
+
+	return num1 / num2, nil
 }
