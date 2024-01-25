ee244172a8d7fa5aa021fab987a38850f1364eb5
Rutik <rutikgagare091971@gmail.com>
2024-01-25 11:58:16 +0530

    added module calci

---
+++ calci.go
@@ -0,0 +1,17 @@
+package calci
+
+func Add(num1 int, num2 int) int {
+	return num1 + num2
+}
+
+func Substract(num1 int, num2 int) int {
+	return num1 - num2
+}
+
+func Multiply(num1, num2 int) int {
+	return num1 * num2
+}
+
+func Divide(num1, num2 int) int {
+	return num1 / num2
+}
